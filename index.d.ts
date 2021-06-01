3aa38a5346bdd0a1a0dd20657c06d400427407ce
jiaxin.lai <goodlaijiaxin@qq.com>
2021-06-01 16:10:05 +0800

    renaming bt* to *

--- index.d.ts
+++ index.d.ts
@@ -4,135 +4,120 @@ declare function instantiate(env: any, buffer: ArrayBuffer): Bullet.instance;
 declare namespace Bullet {
     type ptr = number;
     interface instance {
-        //btVector3
-        btVector3_create(x: number, y: number, z: number): ptr;
-        btVector3_length(p: ptr,): number;
-        btVector3_x(p: ptr,): number;
-        btVector3_y(p: ptr,): number;
-        btVector3_z(p: ptr,): number;
-        btVector3_setX(p: ptr, x: number): void;
-        btVector3_setY(p: ptr, y: number): void;
-        btVector3_setZ(p: ptr, z: number): void;
-        btVector3_setValue(p: ptr, x: number, y: number, z: number): void;
-        btVector3_normalize(p: ptr,): void;
-        // /**[Value] */
-        // btVector3_rotate (p: ptr, wAxis: btVector3, angle: number): btVector3;
-        // btVector3_dot (p: ptr, v: btVector3): number;
-        // btVector3_op_mul (p: ptr, x: number): btVector3;
-        // btVector3_op_add (p: ptr, v: btVector3): btVector3;
-        // btVector3_op_sub (p: ptr, v: btVector3): btVector3;
 
-        //btVector4
-        btVector4_create(x: number, y: number, z: number, w: number): number;
-        btVector4_w(p: ptr): number;
-        setValue(x: number, y: number, z: number): void;
-        setValue(x: number, y: number, z: number, w: number): void;
+        // math
 
-        /**
-         * btQuaternion
-         */
+        Vector3_create(x: number, y: number, z: number): ptr;
+        Vector3_length(p: ptr): number;
+        Vector3_x(p: ptr): number;
+        Vector3_y(p: ptr): number;
+        Vector3_z(p: ptr): number;
+        Vector3_setValue(p: ptr, x: number, y: number, z: number): void;
+        Vector3_normalize(p: ptr): void;
 
-        btQuaternion_x(p: ptr): number;
-        btQuaternion_y(p: ptr): number;
-        btQuaternion_z(p: ptr): number;
-        btQuaternion_w(p: ptr): number;
-        btQuaternion_setX(p: ptr, x: number): void;
-        btQuaternion_setY(p: ptr, y: number): void;
-        btQuaternion_setZ(p: ptr, z: number): void;
-        btQuaternion_setW(p: ptr, w: number): void;
-        btQuaternion_setValue(x: number, y: number, z: number, w: number): void;
-        // btQuaternion_setEulerZYX (z: number, y: number, x: number): void;
-        // btQuaternion_setRotation (axis: btVector3, angle: number): void;
-        // btQuaternion_normalize (): void;
-        // btQuaternion_length2 (): number;
-        // btQuaternion_length (): number;
-        // btQuaternion_dot (q: btQuaternion): number;
-        // /**[Value] */
-        // btQuaternion_normalized (): btQuaternion;
-        // /**[Value] */
-        // btQuaternion_getAxis (): btVector3;
-        // /**[Value] */
-        // btQuaternion_inverse (): btQuaternion;
-        // btQuaternion_getAngle (): number;
-        // btQuaternion_getAngleShortestPath (): number;
-        // btQuaternion_angle (q: btQuaternion): number;
-        // btQuaternion_angleShortestPath (q: btQuaternion): number;
-        // btQuaternion_op_add (q: btQuaternion): btQuaternion;
-        // btQuaternion_op_sub (q: btQuaternion): btQuaternion;
-        // btQuaternion_op_mul (s: number): btQuaternion;
-        // btQuaternion_op_mulq (q: btQuaternion): btQuaternion;
-        // btQuaternion_op_div (s: number): btQuaternion;
+        Quaternion_create(x: number, y: number, z: number, w: number): ptr;
+        Quaternion_x(p: ptr): number;
+        Quaternion_y(p: ptr): number;
+        Quaternion_z(p: ptr): number;
+        Quaternion_w(p: ptr): number;
+        Quaternion_setValue(p: ptr, x: number, y: number, z: number, w: number): void;
 
-        /**
-         * btMatrix3x3 
-         */
+        Transform_create(): ptr;
+        Transform_setIdentity(p: ptr): void;
+        Transform_getOrigin(p: ptr): ptr;
+        Transform_setRotation(p: ptr, quate: ptr): void;
+        Transform_getRotationRef(p: ptr, quat: ptr): void;
 
-        btMatrix3x3_setEulerZYX(ex: number, ey: number, ez: number): void;
-        // btMatrix3x3_getRotation (q: btQuaternion): void;
-        // btMatrix3x3_getRow (y: number): btVector3;
+        // shapes
 
-        /**
-         * btTransform
-         */
+        CollisionShape_isCompound(p: ptr): boolean;
+        CollisionShape_setLocalScaling(p: ptr, scale: ptr): void;
 
-        btTransform_create(): number;
-        btTransform_setIdentity(): void;
-        // btTransform_setOrigin (origin: btVector3): void;
-        // btTransform_setRotation (rotation: btQuaternion): void;
-        // btTransform_getOrigin (): btVector3;
-        // /**[Value] */
-        // btTransform_getRotation (): btQuaternion;
-        // btTransform_getBasis (): btMatrix3x3;
-        // btTransform_setFromOpenGLMatrix (m: number[]): void;
-        // /**[Value] */
-        // btTransform_inverse (): btTransform;
-        // btTransform_op_mul (t: btTransform): btTransform;
+        EmptyShape_create(): ptr;
 
-        // class btMotionState {
-        //     public getWorldTransform (worldTrans: btTransform): void;
-        //     public setWorldTransform (worldTrans: btTransform): void;
-        // }
+        BoxShape_create(p: ptr): ptr;
+        BoxShape_setUnscaledHalfExtents(p: ptr, halfExtents: ptr): void;
 
-        // class btDefaultMotionState extends btMotionState {
-        //     public m_graphicsWorldTrans: btTransform;
-        //     constructor (startTrans?: btTransform, centerOfMassOffset?: btTransform);
-        // }
+        SphereShape_create(radius: number): ptr;
+        SphereShape_setUnscaledRadius(p: ptr, radius: number): void;
 
-        /**
-         * btCollisionObject
-         */
+        StaticPlaneShape_create(normal: ptr, constant: number): ptr;
+        StaticPlaneShape_getPlaneNormal(p: ptr): ptr;
+        StaticPlaneShape_setPlaneConstant(p: ptr, constant: number): void;
 
-        btCollisionObject_create(): number;
-        // btCollisionObject_setAnisotropicFriction(anisotropicFriction: btVector3, frictionMode: number): void;
-        // btCollisionObject_getCollisionShape (): btCollisionShape;
-        btCollisionObject_setContactProcessingThreshold(contactProcessingThreshold: number): void;
-        btCollisionObject_getActivationState(): number;
-        btCollisionObject_setActivationState(newState: number): void;
-        btCollisionObject_forceActivationState(newState: number): void;
-        btCollisionObject_activate(forceActivation?: boolean): void;
-        btCollisionObject_isActive(): boolean;
-        btCollisionObject_isKinematicObject(): boolean;
-        btCollisionObject_isStaticObject(): boolean;
-        btCollisionObject_isStaticOrKinematicObject(): boolean;
-        btCollisionObject_setRestitution(rest: number): void;
-        btCollisionObject_setFriction(frict: number): void;
-        btCollisionObject_setRollingFriction(frict: number): void;
-        // btCollisionObject_getWorldTransform(): btTransform;
-        btCollisionObject_getCollisionFlags(): number;
-        btCollisionObject_setCollisionFlags(flags: number): void;
-        // btCollisionObject_setWorldTransform(worldTrans: btTransform): void;
-        // btCollisionObject_setCollisionShape (collisionShape: btCollisionShape): void;
-        btCollisionObject_setCcdMotionThreshold(ccdMotionThreshold: number): void;
-        btCollisionObject_setCcdSweptSphereRadius(radius: number): void;
-        btCollisionObject_getUserIndex(): number;
-        btCollisionObject_setUserIndex(index: number): void;
-        btCollisionObject_getUserPointer(): number;
-        btCollisionObject_setUserPointer(userPointer: number): void;
+        CompoundShape_create(enableDynamicAabbTree: boolean): ptr;
+        CompoundShape_getNumChildShapes(p: ptr): number;
+        CompoundShape_getChildShape(p: ptr, i: number): ptr;
+        CompoundShape_addChildShape(p: ptr, local: ptr, shape: ptr): void;
+        CompoundShape_removeChildShape(p: ptr, shape: ptr): void;
+        CompoundShape_updateChildTransform(p: ptr, i: number, trans: ptr, shouldRecalculateLocalAabb: boolean): void;
 
-        // shape
-        btEmptyShape_create(): ptr;
-        btBoxShape_create(p: ptr): ptr;
-        btSphereShape_create(radius: number): ptr;
+        // collision
+
+        CollisionObject_create(): number;
+        CollisionObject_getCollisionShape(p: ptr): ptr;
+        CollisionObject_setContactProcessingThreshold(p: ptr, contactProcessingThreshold: number): void;
+        CollisionObject_getActivationState(p: ptr): number;
+        CollisionObject_setActivationState(p: ptr, newState: number): void;
+        CollisionObject_forceActivationState(p: ptr, newState: number): void;
+        CollisionObject_activate(p: ptr, forceActivation?: boolean): void;
+        CollisionObject_isActive(p: ptr): boolean;
+        CollisionObject_isKinematicObject(p: ptr): boolean;
+        CollisionObject_isStaticObject(p: ptr): boolean;
+        CollisionObject_isStaticOrKinematicObject(p: ptr): boolean;
+        CollisionObject_setRestitution(p: ptr, rest: number): void;
+        CollisionObject_setFriction(p: ptr, frict: number): void;
+        CollisionObject_setRollingFriction(p: ptr, frict: number): void;
+        CollisionObject_getWorldTransform(p: ptr): ptr;
+        CollisionObject_getCollisionFlags(p: ptr): number;
+        CollisionObject_setCollisionFlags(p: ptr, flags: number): void;
+        CollisionObject_setWorldTransform(p: ptr, transform: ptr): void;
+        CollisionObject_setCollisionShape(p: ptr, shape: ptr): void;
+        CollisionObject_setCcdMotionThreshold(p: ptr, ccdMotionThreshold: number): void;
+        CollisionObject_setCcdSweptSphereRadius(p: ptr, radius: number): void;
+        CollisionObject_getUserIndex(p: ptr): number;
+        CollisionObject_setUserIndex(p: ptr, index: number): void;
+        CollisionObject_getUserPointer(p: ptr): number;
+        CollisionObject_setUserPointer(p: ptr, userPointer: number): void;
+
+        RigidBodyConstructionInfo_create(m: number, ms: ptr, shape: ptr, localInertia: ptr): ptr;
+        RigidBody_create(p: ptr): ptr;
+        RigidBody_setMassProps(p: ptr, m: number, localInertia: ptr): void;
+        RigidBody_clearState(p: ptr): void;
+        RigidBody_setSleepingThresholds(p: ptr, linear: number, angular: number): void;
+        RigidBody_getMotionState(p: ptr): ptr;
+
+        // dynamic
+
+        DefaultMotionState_create(transform: ptr): ptr;
+        MotionState_getWorldTransform(p: ptr, transform: ptr): void;
+        MotionState_setWorldTransform(p: ptr, transform: ptr): void;
+
+        DefaultCollisionConfiguration_create(): ptr;
+        CollisionDispatcher_create(p: ptr): ptr;
+        Dispatcher_getNumManifolds(p: ptr): number;
+        Dispatcher_getManifoldByIndexInternal(p: ptr, i: number): ptr;
+
+        ManifoldPoint_getShape0(p: ptr): ptr;
+        ManifoldPoint_getShape1(p: ptr): ptr;
+        ManifoldPoint_get_m_index0(p: ptr): number;
+        ManifoldPoint_get_m_index1(p: ptr): number;
+        PersistentManifold_getBody0(p: ptr): ptr;
+        PersistentManifold_getBody1(p: ptr): ptr;
+        PersistentManifold_getNumContacts(p: ptr): number;
+        PersistentManifold_getContactPoint(p: ptr, i: number): ptr;
+
+        DbvtBroadphase_create(): ptr;
+        SequentialImpulseConstraintSolver_create(): ptr;
+
+        CollisionWorld_addCollisionObject(p: ptr, body: ptr, g: number, m: number): void;
+        CollisionWorld_removeCollisionObject(p: ptr, body: ptr): void;
+
+        DiscreteDynamicsWorld_create(dispatcher: ptr, pairCache: ptr, solver: ptr, config: ptr): ptr;
+        DiscreteDynamicsWorld_setGravity(p: ptr, g: ptr): void;
+        DiscreteDynamicsWorld_stepSimulation(p: ptr, timeStep: number, maxSubSteps: number, fixedTimeStep: number): ptr;
+        DiscreteDynamicsWorld_addRigidBody(p: ptr, body: ptr, g: number, m: number): void;
+        DiscreteDynamicsWorld_removeRigidBody(p: ptr, body: ptr): void;
     }
 }
 
